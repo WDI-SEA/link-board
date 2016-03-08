20017dfa6f4ac40d416a00a0f7580fd10c515907
michaelscotthurley <michaelscotthurley>
2016-03-07 19:55:16 -0800

    Assignment complete

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,29 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.authenticate(user_params[:email], user_params[:password])
+    if user
+      #login
+      session[:user_id] = user.id
+      flash[:success] = "You have logged in! Hooray!"
+      redirect_to root_path
+    else  
+      flash[:danger] = "Sorry, incorrect email or password"
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "You have successfully logged out"
+    redirect_to root_path
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
