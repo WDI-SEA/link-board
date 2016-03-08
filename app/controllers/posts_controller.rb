6c0267f9e7b4563ebfe4ff48d52d35feb50ec929
katiosity <lovan.kathy@gmail.com>
2016-03-07 23:36:09 -0800

    added validations in post model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,27 +1,27 @@
 class PostsController < ApplicationController
-  def index
-  	@posts = Post.all
-  end
-
-  def new
-  	@post = Post.new
-  end
-
-  def create
-	Post.create post_params do |u|
-		u.user_id = @current_user.id
-		u.save
+	def index
+		@posts = Post.all
 	end
-	redirect_to root_path
-  end
 
-  def show
-  	@post = Post.find params[:id]
-  end
+	def new
+		@post = Post.new
+	end
 
-  private
+	def create
+		Post.create post_params do |u|
+			u.user_id = @current_user.id
+			u.save
+		end
+		redirect_to root_path
+	end
 
-  def post_params
-  	params.require(:post).permit(:title, :link)
-  end
+	def show
+		@post = Post.find params[:id]
+	end
+
+	private
+
+	def post_params
+  		params.require(:post).permit(:title, :link)
+ 	end
 end
