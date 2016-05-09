e844fb6f0bb40fcbf6d7156baed131bd89e71de6
brytree <brycebortree@gmail.com>
2016-05-09 16:40:05 -0700

    user/authentication is a go

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,15 @@ class User < ActiveRecord::Base
 
   validates :password,
   presence: true,
-  on: :create  
+  on: :create 
+
+  validates :name,
+  presence: true,
+  length: { maximum: 20 }
 
   has_secure_password
+
+  def self.authenticate email, password
+    User.find_by_email(email).try(:authenticate, password)
+  end
 end
