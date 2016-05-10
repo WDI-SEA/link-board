92006cbdddd407af8f7a74c21a7ed511f91756b3
Joshua Granadosin <joshgranadosin@Christabelles-MacBook-Pro.local>
2016-05-09 22:31:32 -0700

    Copy-paste like a BOSS

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,8 @@ class User < ActiveRecord::Base
 
 	validates :name,
 	presence: true,
-	length: {maximum: 20}
+	length: {maximum: 20},
+	uniqueness: {case_sensitive: false}
 
 	has_secure_password
 
