228cff5625c5d584fbd457088aed997a2f3ab8d0
Paul <pacalabre@gmail.com>
2016-05-10 09:35:17 -0700

    Added code to post route

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,16 +5,19 @@ class PostsController < ApplicationController
   end
 
   def create
-    post = Post.create post_params
-    redirect_to root_path
+    post = Post.create post_params do |p|
+      p.user_id = @current_user.user_id
+      p.save
+      redirect_to root_path
   end
 
   def new
+    @post = Post.new
   end
 
   private
   def post_params
-    params.require(:post).permit(:title, :link, :user_id)
+    params.require(:post).permit(:title, :link)
   end
 
 
