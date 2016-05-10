3956e4da415dc6db7187e3ec21e1fa26acd85952
StrongSad <jacobkirkbarrus@gmail.com>
2016-05-10 09:14:00 -0700

    had a really hard time with this hw. Messed something up in the beginning which really set me back

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new user_params
+    user.save!
+    flash[:success] = "User created!"
+    
+    redirect_to root_path
+  end
+
+  private
+
+    def user_params 
+      return params.require(:user).permit(:email, :password, :name)
+    end
+end
