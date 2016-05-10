e9e2673389a8f06ab0bc9d9526260a7170faca58
kendallgoodwin <kendall@kendallgoodwin.com>
2016-05-09 18:38:34 -0700

    signup works

--- link-board/app/controllers/user_controller.rb
+++ link-board/app/controllers/user_controller.rb
@@ -1,9 +1,16 @@
 class UserController < ApplicationController
   def new
+
   end
 
   def create
   	User.create user_params
   	redirect_to root_path
+  end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:name, :email, :password)
   end
 end
