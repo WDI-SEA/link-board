0b359f01cfaefcf11b616eacb3956191ffb018fd
bengus101 <bgustafson29@yahoo.com>
2016-05-09 17:01:43 -0700

    Created 2 models & started adding validations.

---
+++ link-board2/app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+	validates :email,
+	presence: true,
+	uniqueness: {case_sensitive: false}
+
+	validates :password, presence: true, on: :create
+
+	has_secure_password
+end
