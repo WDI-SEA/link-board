55438a81d8abbb90334240402e861436ec19bbdd
derek_robertson <dwilliamrobertson@gmail.com>
2016-07-12 08:45:08 -0700

    set up my routes and auth

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new(post_params)
+    post.user_id = session[:user_id]
+    if post.save
+      flash[:success] = 'Ha you did it'
+      redirect_to root_path
+    else
+      # render json: user.errors.messages
+      flash[:danger] = post.errors.messages
+      redirect_to posts_path
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :link, :user_id)
+  end
+end
