79119d9662b6ba3cceffb01d8f6d0a764422a4ff
Josh McBroom <mcbroomjc@gmail.com>
2016-07-11 18:21:18 -0700

    User model/controller created, routes set up, view for signup created, working on login/sessions

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ActiveRecord::Base
+  has_secure_password
+
+  validates :name,
+  presence: true,
+  length: { in: 1...20 },
+  on: :create
+
+  validates :email, email: true
+
+  validates :password,
+  length: { in: 8..72 },
+  on: :create
+
+  def self.authenticate(params)
+    User.find_by_email(params[:email]).try(:authenticate, params[:password])
+  end
+end
