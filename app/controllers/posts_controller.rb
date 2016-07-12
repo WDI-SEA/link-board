081e31b174ac6b64682e841327156c05fe76aa85
darkenvy <darkenvy6@gmail.com>
2016-07-11 18:57:28 -0700

    Working users & posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.first(limit=10)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new(post_params)
+    if post.save
+      flash[:success] = "Successfully Shared A Link!"
+      redirect_to root_path
+    else
+      flash[:danger] = "Failed to post link. Check your fields"
+      redirect_to posts_path
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :link, :user_id)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+
+  root 'posts#index'
+
+  get 'signup' => 'users#new', as: 'users'
+  post 'signup' => 'users#create'
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
+  get 'posts' => 'posts#new'
+  post 'posts' => 'posts#create'
+
+end
