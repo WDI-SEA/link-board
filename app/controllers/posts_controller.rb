91cc29a073a0602a50289fbe0eb95c8d38dc5f22
Travis Smith <smithtm90@gmail.com>
2016-07-11 20:47:02 -0700

    routes,forms,validations,and authentication are all working

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,27 @@
+class PostsController < ApplicationController
+  def index
+
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new(post_params)
+    post.user_id = session[:user_id]
+    if post.save
+      flash[:success] = "Post created"
+      redirect_to root_path
+    else
+      flash[:danger] = post.errors.messages
+      redirect_to posts_path
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :link, :user_id)
+  end
+end
