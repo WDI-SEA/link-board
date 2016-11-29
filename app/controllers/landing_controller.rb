0a0a1637d1cfd220c9fab7c916d095d703ba31df
Jacob-Elder <jacobelderonly@gmail.com>
2016-11-28 16:22:06 -0800

    fixed current user on landing page

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,8 +1,6 @@
 class LandingController < ApplicationController
-	before_action :is_authenticated, only: [:index]
 	before_action :current_user, only: [:index]
 	
   def index
-  	puts 'hello world'
   end
 end
