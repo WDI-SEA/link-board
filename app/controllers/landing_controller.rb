e7f68bf5229f9cea45ed255b8d3dce54d89fb486
Jacob-Elder <jacobelderonly@gmail.com>
2016-11-28 16:01:53 -0800

    fixed styling for header on all pages

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,4 +1,7 @@
 class LandingController < ApplicationController
+	before_action :is_authenticated, only: [:index]
+	before_action :current_user, only: [:index]
+	
   def index
   	puts 'hello world'
   end
