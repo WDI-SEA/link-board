70bb12f290c057ef5c0e044ba792991fb917d19f
hayes9321 <t.hayes8321@gmail.com>
2016-11-29 14:13:57 -0800

    jt world-wide

---
+++ db/migrate/20161129171045_addupvotes_toposts.rb
@@ -0,0 +1,5 @@
+class AddupvotesToposts < ActiveRecord::Migration[5.0]
+  def change
+  	add_column :posts, :upvotes, :integer, :default => 0
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161129052822) do
+ActiveRecord::Schema.define(version: 20161129171045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20161129052822) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
     t.integer  "downvotes",  default: 0
+    t.integer  "upvotes",    default: 0
     t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
