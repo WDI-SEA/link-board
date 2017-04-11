72df3561e4536b7094bb6b3b993a54bd01948f09
dddotcom <dddotcom@gmail.com>
2017-04-10 22:35:39 -0700

    auth set up

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+  root 'home#index'
+
+  get "login" => 'sessions#new'
+  post "login" => "sessions#create"
+  delete "logout" => "sessions#destroy"
+
+  get "signup" => 'users#new'
+  post 'signup' => "users#create"
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
