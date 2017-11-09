9e24b54413f5467c22043e841e0d605936077389
hsg2007@yahoo.de <YOUR-EMAIL-ADDRESS>
2017-11-08 22:28:26 -0800

    final commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,26 @@
 class UsersController < ApplicationController
-  def sessions
-  end
-
-  def posts
-  end
-
-  def home
-  end
-
-  def comments
-  end
-end
+   def new
+     
+   end
+ 
+   def create
+     user = User.create user_params
+    
+ 
+     if user.id
+       session[:user_id] = user.id
+       flash[:success] = 'User logged in'
+       redirect_to root_path
+     else
+       flash[:danger] = "Invalid credentials"
+       redirect_to login_path
+     end
+   end
+ 
+   # make creation in db
+   private
+ 
+   def user_params 
+     params.require(:user).permit(:name, :email, :password)
+   end
+ end
